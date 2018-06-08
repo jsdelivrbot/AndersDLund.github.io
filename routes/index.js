90c8be2c68c707060049a60cef49efbb1fe1dd6f
AndersDLund <32234005+AndersDLund@users.noreply.github.com>
2018-06-08 16:46:46 -0500

    reverting

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('../index', { title: 'Anders Lund' });
+  res.render('index', { title: 'Anders Lund' });
 });
 
 module.exports = router;
